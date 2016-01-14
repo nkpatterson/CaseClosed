191686ca895bce00d079c310836879d9a8fd1059
Nick Patterson <nkpatterson@live.com>
2016-01-14 14:50:57 +0000

    Added a call to InitialDataBuilder in app_start if no data exists (1st time deploy)

--- src/CaseClosed.Web/Global.asax.cs
+++ src/CaseClosed.Web/Global.asax.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Linq;
 using Abp.Dependency;
 using Abp.Web;
+using CaseClosed.EntityFramework;
+using CaseClosed.Migrations.SeedData;
 using Castle.Facilities.Logging;
 
 namespace CaseClosed.Web
@@ -11,6 +14,16 @@ namespace CaseClosed.Web
         {
             IocManager.Instance.IocContainer.AddFacility<LoggingFacility>(f => f.UseLog4Net().WithConfig("log4net.config"));
             base.Application_Start(sender, e);
+            InitializeData();
+        }
+
+        private void InitializeData()
+        {
+            var context = IocManager.Instance.IocContainer.Resolve<CaseClosedDbContext>();
+            if (!context.Users.Any())
+            {
+                new InitialDataBuilder(context).Build();
+            }
         }
     }
 }
