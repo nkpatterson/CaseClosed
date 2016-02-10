fce27e09600b9a735987443a3e015450f770a2af
Nick Patterson <nkpatterson@live.com>
2016-02-10 13:44:12 +0000

    Deleting deploy directory to avoid MORE confusion

--- deploy/CaseClosed.ResourceGroup/Scripts/stop-webapp.ps1
+++
@@ -1,9 +0,0 @@
-[CmdletBinding()]
-Param(
-   [Parameter(Mandatory=$True)]
-   [string]$AppName
-)
-
-Write-Host "Stopping web app: $AppName"
-Stop-AzureWebsite -Name $AppName
-Write-Host "Done"
--- deploy/start-webapp.ps1
+++
@@ -1,9 +0,0 @@
-[CmdletBinding()]
-Param(
-   [Parameter(Mandatory=$True)]
-   [string]$AppName
-)
-
-Write-Host "Starting web app: $AppName"
-Start-AzureWebsite -Name $AppName
-Write-Host "Done"
