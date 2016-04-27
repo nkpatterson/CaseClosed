b0108f6dd49bb024cb50dafad986052c521a5985
Nick Patterson <nkpatterson@live.com>
2016-04-27 22:19:32 +0000

    Adding a new telemetry initializer for AI.

---
+++ src/CaseClosed.Web/App_Start/CaseClosedTelemetryInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System.Configuration;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace CaseClosed.Web.App_Start
+{
+    public class CaseClosedTelemetryInitializer : ITelemetryInitializer
+    {
+        public void Initialize(ITelemetry telemetry)
+        {
+            var appId = ConfigurationManager.AppSettings["AppInsights.AppIdentifier"];
+            if (!string.IsNullOrEmpty(appId))
+            {
+                telemetry.Context.Properties["AppIdentifier"] = appId;
+            }
+        }
+    }
+}
